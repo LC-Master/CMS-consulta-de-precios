bf0880a362e7fa1199fb6e2c7fc3d7f96a2c83ce
Programador 2 <programador2.lfrq@franquiciadc.interno>
2025-12-01 09:44:16 -0400

    added spatie

---
+++ resources/js/pages/video.tsx
@@ -0,0 +1,29 @@
+import styles from "../../css/input.module.css";
+
+export default function Video() {
+    return (
+        <>
+            <section>
+                <input type="text" className={styles.input_hidden} />
+                <video autoPlay muted draggable={true}>
+                    <source src="/files" type="video/mp4" />
+                </video>
+            </section>
+            <div 
+                onDragOver={(ev) => ev.preventDefault()} 
+                onDrop={(ev) => {
+                    ev.preventDefault();
+                    const file = ev.dataTransfer?.files[0];
+                    if (file) {
+                        console.log('File dropped:', file);
+                    } else {
+                        console.log(file ? "File detected but not valid." : "No file detected.");
+                    }
+                }} 
+                className={styles.container}
+            >
+            </div>
+        </>
+
+    )
+}
--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use Illuminate\Support\Facades\Storage;
 use Inertia\Inertia;
 use Laravel\Fortify\Features;
 
@@ -10,10 +11,18 @@ Route::get('/', function () {
     ]);
 })->name('home');
 
+
 Route::middleware(['auth', 'verified'])->group(function () {
     Route::get('dashboard', function () {
         return Inertia::render('dashboard');
     })->name('dashboard');
 });
 
+Route::get('/video', function () {
+    return Inertia::render('video');
+});
+
+Route::get('/files', function () {
+    return response()->file(Storage::disk('public')->path('1416529-hd_1920_1080_30fps.mp4'));
+});
 require __DIR__.'/settings.php';
