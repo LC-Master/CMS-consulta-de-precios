4e9a67452b580cf2f54097954d71dc52322e76cd
franciso <franciscoantonior30@gmail.com>
2025-12-02 14:00:17 -0400

    Files were added for Device Model

---
+++ app/Models/Device.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Device extends Model
+{
+    /** @use HasFactory<\Database\Factories\DeviceFactory> */
+    use HasFactory;
+
+    protected $fillable = [
+        'center_id',
+        'name',
+        'type',
+        'description',
+        'is_active',
+    ];
+
+    public function center(){
+
+        return $this->belongsTo(Center::class);
+    }
+}
