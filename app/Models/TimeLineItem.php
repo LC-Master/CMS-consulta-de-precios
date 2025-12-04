9d85fd96ca719f8e3b75281b588da12f6085ec8c
franciso <franciscoantonior30@gmail.com>
2025-12-04 13:45:33 -0400

    filleable was added in TimeLineItem

--- app/Models/TimeLineItem.php
+++ app/Models/TimeLineItem.php
@@ -9,4 +9,10 @@ class TimeLineItem extends Model
 {
     /** @use HasFactory<\Database\Factories\TimeLineItemFactory> */
     use HasFactory;
+
+    protected $filleable = [
+        'campaign_id',
+        'media_id',
+        'scheduled_at',
+    ];
 }
