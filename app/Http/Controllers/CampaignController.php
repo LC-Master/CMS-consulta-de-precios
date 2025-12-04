49c3f1cdee13ade8c3b08eead2febb4669514f1a
franciso <franciscoantonior30@gmail.com>
2025-12-04 15:51:04 -0400

    added scroll basic

--- app/Http/Controllers/CampaignController.php
+++ app/Http/Controllers/CampaignController.php
@@ -4,10 +4,10 @@ namespace App\Http\Controllers;
 
 use App\Http\Requests\Campaigns\StoreCampaignRequest;
 use App\Http\Requests\Campaigns\UpdateCampaignRequest;
-use App\Models\Campaign;
-use App\Models\Status;
-use App\Models\Department;
 use App\Models\Agreement;
+use App\Models\Campaign;
+use App\Models\Department;
+use App\Models\Status;
 use Illuminate\Support\Facades\Auth;
 use Inertia\Inertia;
 
@@ -15,48 +15,41 @@ class CampaignController extends Controller
 {
     public function index()
     {
-        $campaigns = Campaign::with(['status', 'department', 'agreement'])
-            ->orderBy('id', 'desc')
-            ->paginate(10);
-
-        /*return Inertia::render('Campaigns/Index', [
-            'campaigns' => $campaigns
-        ]); */
-
-        return Inertia::render('Campaigns/Index', [
-        'campaigns' => Inertia::scroll(fn () => Campaign::paginate())
-    , "others"=> Status::all()]);
+        return Inertia::render('campaign/Index', [
+            'campaigns' => Inertia::scroll(fn () => Campaign::with(['status', 'department', 'agreement'])->paginate()),
+        ]);
     }
 
     public function create()
     {
-        return Inertia::render('Campaigns/Create', [
+        return Inertia::render('campaign/Create', [
             'statuses' => Status::all(),
             'departments' => Department::all(),
             'agreements' => Agreement::all(),
         ]);
     }
 
-   public function store(StoreCampaignRequest $request)
-{
-    $data = $request->validated();
-    
-    Campaign::create(array_merge($data, [
-        'created_by' => Auth::id(),
-    ]));
+    public function store(StoreCampaignRequest $request)
+    {
+        $data = $request->validated();
 
-    return redirect()->route('campaigns.index')->with('success', 'Campaña creada correctamente.');
-}
+        Campaign::create(array_merge($data, [
+            'created_by' => Auth::id(),
+        ]));
+
+        return redirect()->route('campaign.index')->with('success', 'Campaña creada correctamente.');
+    }
 
     public function show(Campaign $campaign)
     {
         $campaign->with(['status', 'department', 'agreement']);
-        return Inertia::render('Campaigns/Show', ['campaign' => $campaign]);
+
+        return Inertia::render('campaign/Show', ['campaign' => $campaign]);
     }
 
     public function edit(Campaign $campaign)
     {
-        return Inertia::render('Campaigns/Edit', [
+        return Inertia::render('campaign/Edit', [
             'campaign' => $campaign,
             'statuses' => Status::all(),
             'departments' => Department::all(),
@@ -72,7 +65,7 @@ class CampaignController extends Controller
 
         $campaign->update($data);
 
-        return redirect()->route('campaigns.index')
+        return redirect()->route('campaign.index')
             ->with('success', 'Campaña actualizada correctamente.');
     }
 
@@ -80,7 +73,7 @@ class CampaignController extends Controller
     {
         $campaign->delete();
 
-        return redirect()->route('campaigns.index')
+            return redirect()->route('campaign.index')
             ->with('success', 'Campaña eliminada.');
     }
 }
