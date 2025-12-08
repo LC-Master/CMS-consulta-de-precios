e0fef076b5a9edabf2d40807739ecae8dd04a216
franciso <franciscoantonior30@gmail.com>
2025-12-08 13:48:46 -0400

    The store method was modified in the campaign controller.

--- resources/js/pages/Campaign/Create.tsx
+++ resources/js/pages/Campaign/Create.tsx
@@ -1,21 +1,31 @@
 import AppLayout from '@/layouts/app-layout'
 import { Form, usePage } from '@inertiajs/react'
+import Select from 'react-select'
 
+interface Center {
+    code: string
+    name: string
+}
 
 export default function CampaignCreate() {
-    const props = usePage().props
-    console.log(props)
+    console.log(usePage().props)
+    const { centers } = usePage<{ centers: Center[] }>().props
+
+    const options = centers.map((center: Center) => {
+        return { value: center.code, label: center.name }
+    })
     return (
         <AppLayout>
-            <Form method='post' action='/campaigns'>
+            <form method='post' className='bg-black' action='/campaigns'>
                 <label htmlFor="title">Título:</label>
                 <input type="text" id="title" name="title" />
                 <label htmlFor="start_date">Fecha y hora (inicio):</label>
                 <input type="datetime-local" id="start_date" name="start_date" />
                 <label htmlFor="end_date">Fecha y hora (fin):</label>
                 <input type="datetime-local" id="end_date" name="end_date" />
-
-            </Form>
+                <label htmlFor="centers">Centros:</label>
+                <Select options={options} id="centers" name="centers" />
+            </form>
         </AppLayout>
     )
 }
