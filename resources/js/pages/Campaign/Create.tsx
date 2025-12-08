79daddff3f19d30194238264c3fe381de695b97f
franciso <franciscoantonior30@gmail.com>
2025-12-08 13:29:27 -0400

    react select added

--- resources/js/pages/Campaign/Create.tsx
+++ resources/js/pages/Campaign/Create.tsx
@@ -1,14 +1,20 @@
 import AppLayout from '@/layouts/app-layout'
-import { Form } from '@inertiajs/react'
+import { Form, usePage } from '@inertiajs/react'
 
 
 export default function CampaignCreate() {
-
+    const props = usePage().props
+    console.log(props)
     return (
         <AppLayout>
             <Form method='post' action='/campaigns'>
-                <label htmlFor="title">Title:</label>
+                <label htmlFor="title">Título:</label>
                 <input type="text" id="title" name="title" />
+                <label htmlFor="start_date">Fecha y hora (inicio):</label>
+                <input type="datetime-local" id="start_date" name="start_date" />
+                <label htmlFor="end_date">Fecha y hora (fin):</label>
+                <input type="datetime-local" id="end_date" name="end_date" />
+
             </Form>
         </AppLayout>
     )
