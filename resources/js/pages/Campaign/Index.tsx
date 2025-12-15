20464229a30bc63d8a0f776e57c25d6e84284100
franciso <franciscoantonior30@gmail.com>
2025-12-15 13:09:32 -0400

    The campaign index was componentized.

--- resources/js/pages/Campaign/Index.tsx
+++ resources/js/pages/Campaign/Index.tsx
@@ -1,15 +1,44 @@
 import { useState } from 'react'
-import { InfiniteScroll, router } from '@inertiajs/react'
+import {  router } from '@inertiajs/react'
 import AppLayout from '@/layouts/app-layout';
 import { useUpdateEffect } from '@/hooks/useUpdateEffect';
-import { Props } from '@/types/campaign/index.types';
+import { Campaign, Props } from '@/types/campaign/index.types';
 import { Filter } from '@/components/Filter';
-
+import { Column } from '@/types/datatable.types';
+import AnchorIcon from '@/components/ui/AnchorIcon';
+import { Eye } from 'lucide-react';
+import { DataTable } from '@/components/DataTable';
 
 
 export default function CampaignsIndex({ campaigns, filters = {}, statuses = [] }: Props) {
     const [search, setSearch] = useState(filters.search || '')
     const [status, setStatus] = useState(filters.status || '')
+    const columns: Column<Campaign>[] = [
+        {
+            key: 'title',
+            header: 'Título',
+            render: (a) => a.title,
+        },
+        {
+            key: 'status',
+            header: 'Estado',
+            render: (a) => a.status.status,
+        },
+        {
+            key: 'created_at',
+            header: 'Creada',
+            render: (a) => a.created_at,
+        },
+        {
+            key: 'actions',
+            header: 'Acciones',
+            render: (a) => (
+                <div className="flex gap-2">
+                    <AnchorIcon url={`/campaign/${a.id}`} icon={Eye} />
+                </div>
+            ),
+        },
+    ]
     useUpdateEffect(() => {
         router.get(
             window.location.pathname,
@@ -17,66 +46,6 @@ export default function CampaignsIndex({ campaigns, filters = {}, statuses = [] 
             { preserveState: true, replace: true, preserveScroll: true }
         )
     }, [search, status])
-    const columns: Column<Agreement>[] = [
-        {
-            key: 'name',
-            header: 'Nombre',
-            render: (a) => a.name,
-        },
-        {
-            key: 'legal_name',
-            header: 'Nombre Legal',
-            render: (a) => a.legal_name,
-        },
-        {
-            key: 'tax_id',
-            header: 'RIF',
-            render: (a) => a.tax_id,
-        }, {
-            key: 'contact_phone',
-            header: 'Teléfono',
-            render: (a) => a.contact_phone,
-        }, {
-            key: 'contact_person',
-            header: 'Persona Contacto',
-            render: (a) => a.contact_person,
-        },
-        {
-            key: 'status',
-            header: 'Estatus',
-            render: (a) => (
-                <span
-                    className={`px-2 inline-flex text-xs font-semibold rounded-full ${Number(a.is_active)
-                        ? 'bg-green-100 text-green-800'
-                        : 'bg-red-100 text-red-800'
-                        }`}
-                >
-                    {Number(a.is_active) ? 'Activo' : 'Inactivo'}
-                </span>
-            ),
-        },
-        {
-            key: 'actions',
-            header: 'Acciones',
-            render: (a) => (
-                <div className="flex gap-2">
-                    <a
-                        href={`/agreement/${a.id}`}
-                        className="p-2 bg-locatel-medio text-white rounded-md"
-                    >
-                        <Eye className="w-4 h-4" />
-                    </a>
-
-                    <a
-                        href={`/agreement/${a.id}/edit`}
-                        className="p-2 bg-locatel-oscuro text-white rounded-md"
-                    >
-                        <Pencil className="w-4 h-4" />
-                    </a>
-                </div>
-            ),
-        },
-    ]
     return (
         <AppLayout>
             <div className="space-y-4 px-4 pb-4">
@@ -103,8 +72,14 @@ export default function CampaignsIndex({ campaigns, filters = {}, statuses = [] 
                             ],
                             onChange: setStatus,
                         },
-
                     ]}
+                />
+
+                <DataTable
+                    data={campaigns.data}
+                    columns={columns}
+                    rowKey={(a) => a.id}
+                    infiniteData="campaigns"
                 />
 
             </div>
