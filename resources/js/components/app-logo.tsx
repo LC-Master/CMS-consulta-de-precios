b001a1dd5089d376f0f77826c35e62e1120a21a9
benpulido21 <benpulido6@gmail.com>
2025-12-18 11:00:43 -0400

    logo switch sidebar

--- resources/js/components/app-logo.tsx
+++ resources/js/components/app-logo.tsx
@@ -1,15 +1,41 @@
+import { useSidebar } from "@/components/ui/sidebar";
 
 export default function AppLogo() {
+    const { state } = useSidebar();
+    const isCollapsed = state === "collapsed";
+
     return (
         <>
-            <div className="flex aspect-square size-24 items-center justify-center rounded-md text-sidebar-primary-foreground">
-                <img src="/Logo.webp" alt="CMS Locatel Logo" width={94} height={94} fetchPriority="high"/>
-            </div>
-            <div className="ml-1 grid flex-1 text-left text-sm">
-                <span className="mb-0.5 truncate leading-tight font-semibold">
-                    CMS Locatel
-                </span>
-            </div>
+            {isCollapsed ? (
+
+                <div className="flex items-center justify-center w-full">
+                    <img 
+                        src="/Logo2.webp" 
+                        alt="CMS Locatel Icon" 
+                        className="size-8 object-contain" 
+                    />
+                </div>
+            ) : (
+
+                <>
+                    {/*  */}
+                    <div className="flex aspect-square size-24 items-center justify-center rounded-md text-sidebar-primary-foreground">
+                        <img 
+                            src="/Logo.webp" 
+                            alt="CMS Locatel Logo" 
+                            width={94} 
+                            height={94} 
+                            className="object-contain"
+                            fetchPriority="high"
+                        />
+                    </div>
+                    <div className="ml-1 grid flex-1 text-left text-sm">
+                        <span className="mb-0.5 truncate leading-tight font-semibold text-gray-700">
+                            CMS Locatel
+                        </span>
+                    </div>
+                </>
+            )}
         </>
     );
 }
